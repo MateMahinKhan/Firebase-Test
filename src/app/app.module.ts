83818d8b012345bb82207807b06e7ae23cf44baa
Mahin Khan <kuttareturns@gmail.com>
2017-10-30 07:36:32 +0500

    chore: removed unnecessary import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
@@ -14,7 +13,6 @@ import { DataService } from './services/data.service';
   ],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
     BrowserModule
   ],
   providers: [ AngularFireDatabase, DataService ],
